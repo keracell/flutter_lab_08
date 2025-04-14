cd3def84efff55c92b4846622970f6bbf76b3680
Agil Kerimov <ahkerimov@gmail.com>
2025-04-15 00:20:36 +0400

    Update theme fonts and add CategoriesScreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lab_08_meals/screens/categories_screen.dart';
 
-final textTheme = GoogleFonts.latoTextTheme();
-final themeLight = ThemeData(
+final textTheme = GoogleFonts.robotoTextTheme();
+final themeLight = ThemeData.dark().copyWith(
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
@@ -19,9 +20,7 @@ void main() {
       appBar: AppBar(
         title: const Text('Meals App'),
       ),
-      body: const Center(
-        child: Text('ultra'),
-      ),
+      body: const CategoriesScreen(),
     ),
   ));
 }
